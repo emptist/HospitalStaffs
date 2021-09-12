49368e35327652e1d04cfc75ed81654d13b39504
emptist <jigme1968@gmail.com>
2021-09-12 22:15:17 +0800

    Initial Commit

---
+++ Shared/HospitalStaffsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HospitalStaffsApp.swift
+//  Shared
+//
+//  Created by jk on 2021/9/12.
+//
+
+import SwiftUI
+
+@main
+struct HospitalStaffsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
